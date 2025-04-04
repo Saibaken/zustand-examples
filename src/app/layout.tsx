d4c41f788183551c2561d45b973c92d3d2fb2493
Saibaken <shelamov@vk.com>
2025-04-04 16:38:27 +0300

    Add 'Home' link on all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { Home } from "lucide-react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 import Script from "next/script";
 import "./globals.css";
 
@@ -33,6 +36,12 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-screen w-screen`}
       >
         {children}
+        <Link href="/">
+          <Button className="fixed right-5 bottom-5 cursor-pointer">
+            <Home />
+            Go home
+          </Button>
+        </Link>
       </body>
     </html>
   );
