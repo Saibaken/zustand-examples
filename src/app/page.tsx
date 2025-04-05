71b7715badff0c4973aafcdf9ede1287f4cccb05
Saibaken <shelamov@vk.com>
2025-04-05 11:46:29 +0300

    Change cards headers and descriptions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import Link from "next/link";
 
-const links: { link: string; title: string }[] = [
-  { link: "/basic", title: "Basic example" },
-  { link: "/immer", title: "Immer example" },
-  { link: "/persist", title: "Persist example" },
-  { link: "/async", title: "Async store example" },
-  { link: "/context", title: "Context store example" },
+const links: { link: string; title: string; description: string }[] = [
+  {
+    link: "/basic",
+    title: "Базовый пример",
+    description: "Базовый пример с несколькими полями и экшенами",
+  },
+  {
+    link: "/immer",
+    title: "Immer middleware",
+    description:
+      "Использование immer middleware для изменений сложных объектов иммутабельно",
+  },
+  {
+    link: "/persist",
+    title: "Persist middleware",
+    description:
+      "Использование persist middleware для хранения и синхронизации с JSON хранилищем.",
+  },
+  {
+    link: "/async",
+    title: "Async set",
+    description: "Асинхронный вызов изменений стора",
+  },
+  {
+    link: "/context",
+    title: "React context и фабрики",
+    description: "Фабрика хранилищ и использование в контексте",
+  },
 ];
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full w-full bg-slate-800 text-white">
-      <div className="grid grid-cols-2 gap-8 w-96">
-        {links.map(({ link, title }) => (
+      <div className="grid grid-cols-2 gap-8 w-1/2">
+        {links.map(({ link, title, description }) => (
           <Link href={link} key={link}>
-            <Card>
+            <Card className="border-2 border-slate-400 h-full w-full">
               <CardHeader>
                 <CardTitle>{title}</CardTitle>
+                <CardDescription>{description}</CardDescription>
               </CardHeader>
             </Card>
           </Link>
